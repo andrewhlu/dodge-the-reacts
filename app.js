0642c9b87efe725f87776e92080ceb49c334a5e5
andrewhlu <alu@ucsb.edu>
2019-10-26 00:52:50 -0700

    Set up connection to cockroachdb

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const asnyc = require('async');
+const fs = require('fs');
+const pg = require('pg');
+
+app.use('/static', express.static('static'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -21,6 +28,28 @@ io.on('connection', function(socket){
 
 // io.emit('some event', { for: 'everyone' });
 
+
+// Database configuration
+var config = {
+  user: "gridiron",
+  host: "gcp-us-west1.gridiron-gauntlet.crdb.io",
+  database: 'defaultdb',
+  port: 26257
+  // sslmode: "require",
+  // ssl: {}
+};
+
+// Setting up the database
+var pool = new pg.Pool(config);
+
+pool.connect((error, client, done) => {
+  
+});
+
+
+
+
+// This is a blocking function, keep this at the end!
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
