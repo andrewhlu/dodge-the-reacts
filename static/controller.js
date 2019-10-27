4471c56399909748fd98c0a4f906c56eabab0971
andrewhlu <alu@ucsb.edu>
2019-10-27 03:30:15 -0700

    Add logic for limited spawns in a given time period

--- static/controller.js
+++ static/controller.js
@@ -1,6 +1,22 @@
 var introDiv = document.getElementById("intro-div");
+var introNumSpawnsSpan = document.getElementById("intro-num-spawns");
+var introSpawnTimeDiv = document.getElementById("intro-spawn-time");
+
 var gameDiv = document.getElementById("game-div");
+var timerSpan = document.getElementById("game-timer");
+var livesSpan = document.getElementById("game-lives-timer");
+var enemiesSpan = document.getElementById("game-enemies-left");
+
 var socket = io();
+
+var spawnsRemaining = 2;
+var spawnTimeout = 2; // in seconds
+
+function init() {
+    introNumSpawnsSpan.innerHTML = spawnsRemaining;
+    introSpawnTimeDiv.innerHTML = spawnTimeout;
+    enemiesSpan.innerHTML = spawnsRemaining;
+}
 
 function joinGame() {
     introDiv.setAttribute("hidden", true);
@@ -8,28 +24,45 @@ function joinGame() {
 }
 
 function spawnEnemy(id) {
-    var splitId = id.split("");
+    if(spawnsRemaining > 0) {
+        var splitId = id.split("");
     
-    var enemy = {
-        pos: splitId[1]
-    };
+        var enemy = {
+            pos: splitId[1]
+        };
 
-    switch(splitId[0]) {
-        case "L":
-            enemy.location = "left";
-            break;
-        case "T":
-            enemy.location = "top";
-            break;
-        case "R":
-            enemy.location = "right";
-            break;
-        case "B":
-            enemy.location = "bottom";
-            break;
-        default:
-            console.log("An error occurred!");
+        switch(splitId[0]) {
+            case "L":
+                enemy.location = "left";
+                break;
+            case "T":
+                enemy.location = "top";
+                break;
+            case "R":
+                enemy.location = "right";
+                break;
+            case "B":
+                enemy.location = "bottom";
+                break;
+            default:
+                console.log("An error occurred!");
+        }
+
+        spawnsRemaining--;
+        enemiesSpan.innerHTML = spawnsRemaining;
+
+        socket.emit('enemy', enemy);
+
+        setTimeout(() => {
+            spawnsRemaining++;
+            enemiesSpan.innerHTML = spawnsRemaining;
+        }, spawnTimeout * 1000);
     }
-
-    socket.emit('enemy', enemy);
+    else {
+        console.log("No spawns remaining!");
+    }   
 }
+
+window.onload = () => {
+    init();
+};
