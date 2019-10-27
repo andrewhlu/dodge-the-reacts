890c1dc22c591678f6882a5fc19616c1c4df6d41
andrewhlu <alu@ucsb.edu>
2019-10-27 02:30:30 -0700

    Add field and spawn buttons to controller

--- static/controller.js
+++ static/controller.js
@@ -4,6 +4,32 @@ var socket = io();
 
 function joinGame() {
     introDiv.setAttribute("hidden", true);
-    gameDiv.removeAttribute("hidden")
+    gameDiv.removeAttribute("hidden");
+}
 
+function spawnEnemy(id) {
+    var splitId = id.split("");
+    
+    var enemy = {
+        pos: splitId[1]
+    };
+
+    switch(splitId[0]) {
+        case "L":
+            enemy.location = "left";
+            break;
+        case "T":
+            enemy.location = "top";
+            break;
+        case "R":
+            enemy.location = "right";
+            break;
+        case "B":
+            enemy.location = "bottom";
+            break;
+        default:
+            console.log("An error occurred!");
+    }
+
+    socket.emit('enemy', enemy);
 }
